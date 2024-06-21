e68fbf69de79ede496a43e91b19b27ad2e6c1cc4
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-21 17:49:38 -0500

    pedimos al usuario el numero de productos

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         System.out.println("Productos");
+
+        // pedimos al usuario el numero de productos
+        int cantidadProductos;
+        System.out.print("Ingrese la cantidad de productos: ");
+        cantidadProductos = leer.nextInt();
         
     }
 }
